bfcbb4a7cc0504562a7a3c09a02e88143752c6a2
Hector Castro <hectcastro@gmail.com>
2021-06-02 20:55:29 -0400

    Add verification for Shopify webhooks

--- lib/index.ts
+++ lib/index.ts
@@ -9,6 +9,7 @@ export default function main(app: sst.App): void {
       TRELLO_API_KEY: process.env.TRELLO_API_KEY ?? "",
       TRELLO_OAUTH_TOKEN: process.env.TRELLO_OAUTH_TOKEN ?? "",
       TRELLO_LIST_ID: process.env.TRELLO_LIST_ID ?? "",
+      SHOPIFY_WEBHOOK_SECRET: process.env.SHOPIFY_WEBHOOK_SECRET ?? "",
     },
   });
 
--- src/lambda.ts
+++ src/lambda.ts
@@ -1,31 +1,42 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyHandlerV2 } from "aws-lambda";
 import { Order } from "./model/order";
 import axios from "axios";
+import crypto from "crypto";
 
-const client = axios.create({
+const TRELLO_OAUTH_TOKEN = process.env.TRELLO_OAUTH_TOKEN ?? "";
+const TRELLO_API_KEY = process.env.TRELLO_API_KEY ?? "";
+const TRELLO_LIST_ID = process.env.TRELLO_LIST_ID ?? "";
+const SHOPIFY_WEBHOOK_SECRET = process.env.SHOPIFY_WEBHOOK_SECRET ?? "";
+
+const CLIENT = axios.create({
   baseURL: "https://api.trello.com/1",
   params: {
-    key: process.env.TRELLO_API_KEY ?? "",
-    token: process.env.TRELLO_OAUTH_TOKEN ?? "",
+    key: TRELLO_API_KEY,
+    token: TRELLO_OAUTH_TOKEN,
   },
 });
+
+function verifyWebhook(body: string, hmacSha: string): boolean {
+  const computedHmacSha = crypto
+    .createHmac("sha256", SHOPIFY_WEBHOOK_SECRET)
+    .update(body, "utf8")
+    .digest("base64");
+
+  return computedHmacSha == hmacSha;
+}
 
 function createTrelloCardName(order: Order): string {
   return `${order.name}: ${order.customer?.first_name} ${order.customer?.last_name} ($${order.total_price})`;
 }
 
-export const handler: APIGatewayProxyHandlerV2 = async (
-  event: APIGatewayProxyEventV2
-) => {
-  const shopifyOrder: Order = JSON.parse(event.body ?? "");
-
-  const card = await client.post("/cards", {
-    idList: process.env.TRELLO_LIST_ID,
+async function createTrelloCard(shopifyOrder: Order) {
+  const card = await CLIENT.post("/cards", {
+    idList: TRELLO_LIST_ID,
     name: createTrelloCardName(shopifyOrder),
     pos: "bottom",
   });
 
-  const checklist = await client.post("/checklists", {
+  const checklist = await CLIENT.post("/checklists", {
     idCard: card.data.id,
     name: "Order Items",
     pos: "bottom",
@@ -33,13 +44,28 @@ export const handler: APIGatewayProxyHandlerV2 = async (
 
   await Promise.all(
     shopifyOrder.line_items?.map(async (lineItem) => {
-      await client.post(`/checklists/${checklist.data.id}/checkItems`, {
+      await CLIENT.post(`/checklists/${checklist.data.id}/checkItems`, {
         id: checklist.data.id,
         name: `${lineItem.quantity} x ${lineItem.name}`,
         pos: "bottom",
       });
     }) ?? []
   );
+}
+
+export const handler: APIGatewayProxyHandlerV2 = async (
+  event: APIGatewayProxyEventV2
+) => {
+  if (
+    !verifyWebhook(
+      event.body ?? "",
+      event.headers["x-shopify-hmac-sha256"] ?? ""
+    )
+  ) {
+    return { statusCode: 401 };
+  }
+
+  await createTrelloCard(JSON.parse(event.body ?? ""));
 
   return {
     statusCode: 200,
