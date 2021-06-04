6a1d0d16850aa005a467f168cd7f9b629d4be910
Hector Castro <hectcastro@gmail.com>
2021-06-03 21:01:47 -0400

    Use Node.js v14.x in Lambda and CI

--- lib/index.ts
+++ lib/index.ts
@@ -1,16 +1,17 @@
-import MyStack from "./MyStack";
+import * as lambda from "@aws-cdk/aws-lambda";
 import * as sst from "@serverless-stack/resources";
+import MyStack from "./MyStack";
 
 export default function main(app: sst.App): void {
   // Set default runtime for all functions
   app.setDefaultFunctionProps({
-    runtime: "nodejs12.x",
     environment: {
       TRELLO_API_KEY: process.env.TRELLO_API_KEY ?? "",
       TRELLO_OAUTH_TOKEN: process.env.TRELLO_OAUTH_TOKEN ?? "",
       TRELLO_LIST_ID: process.env.TRELLO_LIST_ID ?? "",
       SHOPIFY_WEBHOOK_SECRET: process.env.SHOPIFY_WEBHOOK_SECRET ?? "",
     },
+    runtime: lambda.Runtime.NODEJS_14_X,
   });
 
   new MyStack(app, "my-stack");
