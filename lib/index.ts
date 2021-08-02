6bdab9438fb9fcbd94d9069d0aa4fc9f80b21a1d
Hector Castro <hectcastro@gmail.com>
2021-08-02 07:35:51 -0400

    fix: extend function timeout to 15 seconds

--- lib/index.ts
+++ lib/index.ts
@@ -5,6 +5,7 @@ import MyStack from "./MyStack";
 export default function main(app: sst.App): void {
   app.setDefaultFunctionProps({
     runtime: lambda.Runtime.NODEJS_14_X,
+    timeout: 15,
   });
 
   new MyStack(app, "my-stack");
