bdcb6d02cf23dcada7fffc27f621ddef513595fd
Hector Castro <hectcastro@gmail.com>
2022-05-04 07:39:00 -0400

    fix: sst.Function runtime and architecture compat

--- stacks/index.ts
+++ stacks/index.ts
@@ -1,12 +1,11 @@
-import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as sst from "@serverless-stack/resources";
 import MyStack from "./MyStack";
 
 export default function main(app: sst.App): void {
   app.setDefaultFunctionProps({
-    runtime: lambda.Runtime.NODEJS_14_X,
+    runtime: "nodejs14.x",
     timeout: 15,
-    architecture: lambda.Architecture.ARM_64,
+    architecture: "arm_64",
   });
 
   new MyStack(app, "my-stack");
